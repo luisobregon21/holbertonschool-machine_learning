8e0d743fb1b638ced3b2941840d72deea0371135
luisobregon21 <luisitonicaragua2002@gmail.com>
2022-07-25 14:51:23 -0400

    add(0x00-linear_algebra): new task added
    function returns the sum two matrices.
    You can assume that mat1 and mat2 are 2D matrices containing ints/floats
    
    You can assume all elements in the same dimension are of the same type/shape
    
    If mat1 and mat2 are not the same shape, return None

---
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+def add_matrices2D(mat1, mat2):
+    ''' Add two matrices of integers '''
+
+    sum = []
+
+    if len(mat1[0]) != len(mat2[0]):
+        return None
+
+    for idx in range(len(mat1)):
+        newList = []
+        for idx2 in range(len(mat1[idx])):
+            newList.append((mat1[idx][idx2] + mat2[idx][idx2]))
+        sum.append(newList)
+    return sum
