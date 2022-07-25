22d669e7dacaa9df5fb8e3aa5e7d2af3bdf9ddd9
luisobregon21 <luisitonicaragua2002@gmail.com>
2022-07-25 15:55:07 -0400

    add(0x00-linear_algebra): new task added
    function concatenates twio matrices

---
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    ''' function concatenates 2 matrices' along a specific axis '''
+    newMatrix = []
+    if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
+        for row in mat1:
+            newMatrix.append(row)
+        for row in mat2:
+            newMatrix.append(row)
+    elif axis == 1:
+        if len(mat1) != len(mat2):
+            return None
+        for row1, row2 in zip(mat1, mat2):
+            newMatrix.append(row1 + row2)
+    return newMatrix
