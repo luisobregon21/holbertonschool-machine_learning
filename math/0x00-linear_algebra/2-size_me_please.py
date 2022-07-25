1a936c8405a234aaa7230e06984b40b0d64891f4
luisobregon21 <luisitonicaragua2002@gmail.com>
2022-07-25 13:56:26 -0400

    add(0x00-linear_algebra): matrix shape function
    function calculates the shape of matrix

---
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def matrix_shape(matrix):
+    ''' function calculates the shape of a matrix '''
+    shape = []
+    while type(matrix) == list:
+        shape.append(len(matrix))
+        matrix = matrix[0]
+
+    return shape
