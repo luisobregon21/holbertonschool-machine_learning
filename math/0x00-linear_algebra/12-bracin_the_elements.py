8ece924faad666573cff2eff50b2f22ca8678215
Luis Obregon <luisitonicaragua2002@gmail.com>
2022-07-25 19:22:05 -0400

    add(0x00-linear_algebra): add new task
    function performs element-wise addition, subtraction, multiplication, and division

---
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''performs element-wise addition, subtraction, multiplication, and division'''
+
+
+def np_elementwise(mat1, mat2):
+    '''
+    function performs element-wise addition,
+    subtraction, multiplication, and division
+    '''
+    return(mat1+mat2, mat1-mat2, mat1*mat2, mat1/mat2)
