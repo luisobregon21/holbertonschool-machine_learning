6bf33a89ad1c36f99036a8622dd513cd3d17d5b3
luisobregon21 <luisitonicaragua2002@gmail.com>
2022-07-25 14:34:55 -0400

    add(0x00-linear_algebra): adds two arrays element
    returns new list

---
+++ math/0x00-linear_algebra/4-line_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def add_arrays(arr1, arr2):
+    '''
+    Add two arrays of integers
+    '''
+    if len(arr1) != len(arr2):
+        return None
+    else:
+        return [arr1[i] + arr2[i] for i in range(len(arr1))]
