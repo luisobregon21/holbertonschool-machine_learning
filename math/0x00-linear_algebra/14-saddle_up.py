a1f3e9ede8992d0f055c532395d65c45fa06fa06
Luis Obregon <luisitonicaragua2002@gmail.com>
2022-07-25 19:32:39 -0400

    add(0x00-linear_algebra): add new task
    perform matrix multiplication using numpy lib

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+''' perform matrix multiplication '''
+
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    '''function performs matrix multiplication'''
+    return np.matmul(mat1, mat2)
