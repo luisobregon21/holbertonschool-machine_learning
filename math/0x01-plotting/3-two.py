9edc8c9cc3b76e38525bafbcc2a87fca1f4f7045
Luis Obregon <luisitonicaragua2002@gmail.com>
2022-08-01 19:43:39 -0400

    add(0x01-plotting): add task 3

--- math/0x01-plotting/3-two.py
+++ math/0x01-plotting/3-two.py
@@ -10,4 +10,12 @@ t2 = 1600
 y1 = np.exp((r / t1) * x)
 y2 = np.exp((r / t2) * x)
 
-# your code here
+plt.plot(x, y1, 'r--', label='C-14')
+plt.plot(x, y2, 'g-', label='Ra-226')
+plt.xlabel('Time (years)')
+plt.ylabel('Fraction Remaining')
+plt.title('Exponential Decay of Radioactive Elements')
+plt.legend()
+plt.xlim((0, 20000))
+plt.ylim((0, 1))
+plt.show()
