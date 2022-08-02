7ed6172542d0d4adcd466e6074570357d025c795
Luis Obregon <luisitonicaragua2002@gmail.com>
2022-08-01 22:20:15 -0400

    add(0x01-plotting): add advance task 101

--- math/0x01-plotting/101-pca.py
+++ math/0x01-plotting/101-pca.py
@@ -13,4 +13,13 @@ norm_data = data - data_means
 _, _, Vh = np.linalg.svd(norm_data)
 pca_data = np.matmul(norm_data, Vh[:3].T)
 
-# your code here
+figure = plt.figure()
+axis = figure.add_subplot(1, 1, 1, projection='3d')
+axis.scatter(pca_data[:, 0], pca_data[:, 1], pca_data[:, 2],
+             c=labels, cmap=plt.get_cmap('plasma'),
+             label=labels)
+axis.set_xlabel('U1')
+axis.set_ylabel('U2')
+axis.set_zlabel('U3')
+plt.title('PCA of Iris Dataset')
+plt.show()
