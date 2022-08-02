b5999819ba25a52459166ddc7e932c689f642a8e
Luis Obregon <luisitonicaragua2002@gmail.com>
2022-08-01 20:38:29 -0400

    add(0x01-plotting): add task 4 and 5

--- math/0x01-plotting/4-frequency.py
+++ math/0x01-plotting/4-frequency.py
@@ -5,5 +5,14 @@ import matplotlib.pyplot as plt
 
 np.random.seed(5)
 student_grades = np.random.normal(68, 15, 50)
-
-# your code here
+bins = []
+for x in range(11):
+    bins.append(x*10)
+plt.hist(student_grades, bins=bins, edgecolor='black')
+plt.xlabel('Grades')
+plt.ylabel('Number of Students')
+plt.title('Project A')
+plt.xlim(0, 100)
+plt.xticks(np.arange(0, 101, 10))
+plt.ylim(0, 30)
+plt.show()
--- math/0x01-plotting/5-all_in_one.py
+++ math/0x01-plotting/5-all_in_one.py
@@ -26,4 +26,53 @@ y32 = np.exp((r3 / t32) * x3)
 np.random.seed(5)
 student_grades = np.random.normal(68, 15, 50)
 
-# your code here
+fig = plt.figure()
+
+# line graph
+axis1 = fig.add_subplot(3, 2, 1)
+axis1.plot(y0, '-r')
+axis1.set_xlim((0, 10))
+fig.suptitle('All in One')
+
+# scatter plot
+axis2 = fig.add_subplot(3, 2, 2)
+axis2.scatter(x1, y1, c='m')
+axis2.set_xlabel('Height (in)', fontsize='x-small')
+axis2.set_ylabel('Weight (lbs)', fontsize='x-small')
+axis2.set_title("Men's Height vs Weight", fontsize='x-small')
+
+# x ↦ y as a line graph
+axis3 = fig.add_subplot(3, 2, 3)
+axis3.plot(x2, y2)
+axis3.set_xlabel('Time (years)', fontsize='x-small')
+axis3.set_ylabel('Fraction Remaining', fontsize='x-small')
+axis3.set_title('Exponential Decay of C-14', fontsize='x-small')
+axis3.set_yscale('log')
+axis3.set_xlim((0, 28650))
+
+# x ↦ y1 and x ↦ y2 line graphs
+axis4 = fig.add_subplot(3, 2, 4)
+axis4.plot(x3, y31, 'r--', label='C-14')
+axis4.plot(x3, y32, 'g-', label='Ra-226')
+axis4.set_xlabel('Time (years)', fontsize='x-small')
+axis4.set_ylabel('Fraction Remaining', fontsize='x-small')
+axis4.set_title('Exponential Decay of Radioactive Elements',
+                fontsize='x-small')
+axis4.legend()
+axis4.set_xlim((0, 20000))
+axis4.set_ylim((0, 1))
+
+bins = []
+for x in range(11):
+    bins.append(x*10)
+axis5 = fig.add_subplot(3, 1, 3)
+axis5.hist(student_grades, bins=bins, edgecolor='black')
+axis5.set_xlabel('Grades', fontsize='x-small')
+axis5.set_ylabel('Number of Students', fontsize='x-small')
+axis5.set_title('Project A', fontsize='x-small')
+axis5.set_xlim(0, 100)
+axis5.set_xticks(np.arange(0, 101, 10))
+axis5.set_ylim(0, 30)
+
+plt.tight_layout()
+plt.show()
