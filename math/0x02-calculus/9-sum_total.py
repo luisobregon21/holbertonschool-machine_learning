5f2e2340e69ebb9127f7dfef1b8da2e698f280f3
Luis Obregon <luisitonicaragua2002@gmail.com>
2022-08-03 22:56:56 -0400

    seperated if conditions

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -10,6 +10,7 @@ def summation_i_squared(n):
 
     if not isinstance(n, int) or n < 1:
         return None
-    elif n == 1:
+
+    if n == 1:
         return 1
     return int((n**2) + (summation_i_squared(n-1)))
