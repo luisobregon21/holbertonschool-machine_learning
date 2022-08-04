b16caed0a3ed4b5555ff6d5ccc15733229120d8b
Luis Obregon <luisitonicaragua2002@gmail.com>
2022-08-03 23:16:24 -0400

    add(0x02-calculus): modify loop

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -9,16 +9,13 @@ def poly_derivative(poly):
     coefficient belongs to
     example: if f(x) = x^3 + 3x +5, poly is equal to [5, 3, 0, 1]
     '''
-    if not isinstance(poly, list) or len(poly) < 1 or poly is None:
+    if not isinstance(poly, list) or len(poly) < 1:
         return None
 
-    if not any(isinstance(val, int) for val in poly):
-        return None
     derivative = []
-    for power, coefficient in enumerate(poly):
-        if power == 0:
-            derivative.append(0)
-        if power == 1:
-            derivative = []
-        derivative.append(power * coefficient)
+
+    for power in range(1, len(poly)):
+        if type(poly[power]) is not int:
+            return None
+        derivative.append(poly[power]*power)
     return derivative
