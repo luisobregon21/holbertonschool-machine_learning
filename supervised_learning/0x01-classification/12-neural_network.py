0ab3b562b409d90b632adc6addb8cdf4a4f8fc26
luisobregon21 <luisitonicaragua2002@gmail.com>
2022-08-20 15:46:11 -0400

    add(supervised-learning): task 12
    add public method that evaluates NN predictions

---
+++ supervised_learning/0x01-classification/12-neural_network.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+''' NeuralNetwork that defines a neural network '''
+import numpy as np
+
+
+class NeuralNetwork():
+    '''
+    NeuralNetwork class defines a neural network
+    with one hidden layer performing binary classification
+    '''
+
+    def __init__(self, nx, nodes):
+        '''
+        class constructor.
+        :nx: is the number of input features
+        :nodes: is the number of nodes found
+        in the hidden layer
+
+        '''
+
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+
+        if type(nodes) is not int:
+            raise TypeError('nodes must be an integer')
+        if nodes < 1:
+            raise ValueError('nodes must be a positive integer')
+
+        self.__W1 = np.random.normal(size=(nodes, nx))
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.normal(size=(1, nodes))
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        '''
+        W1: The weights vector for the hidden layer.
+        '''
+        return self.__W1
+
+    @property
+    def b1(self):
+        '''
+        b1: The bias for the hidden layer.
+        '''
+        return self.__b1
+
+    @property
+    def A1(self):
+        '''
+        A1: The activated output for the hidden layer.
+        '''
+        return self.__A1
+
+    @property
+    def W2(self):
+        '''
+        W2: The weights vector for the output neuron.
+        '''
+        return self.__W2
+
+    @property
+    def b2(self):
+        '''
+        b2: The bias for the output neuron.
+        '''
+        return self.__b2
+
+    @property
+    def A2(self):
+        '''
+        A2: The activated output for the output neuron (prediction).
+        '''
+        return self.__A2
+
+    def forward_prop(self, X):
+        '''
+        Calculates the forward propagation of the neural network
+        :X: is a numpy.ndarray with shape (nx, m) that contains
+        the input data
+        nx: is the number of input features to the neuron
+        m: is the number of examples
+        '''
+        fwp = np.matmul(self.__W1, X) + self.__b1
+        self.__A1 = 1/(1+np.exp(-fwp))
+
+        self.__A2 = 1 / \
+            (1 + np.exp(-(np.matmul(self.__W2, self.__A1) + self.__b2)))
+        return self.__A1, self.__A2
+
+    def cost(self, Y, A):
+        '''
+        Calculates the cost of the model using logistic regression
+        :Y: is a numpy.ndarray with shape (1, m) that contains
+        the correct labels for the input data
+        :A: is a numpy.ndarray with shape (1, m) containing
+        the activated output of the neuron for each example
+        '''
+        m = Y.shape[1]
+        cost = -1/m * np.sum(Y * np.log(A) + (1-Y) * (np.log(1.0000001 - A)))
+        return cost
+
+    def evaluate(self, X, Y):
+        '''
+        Evaluates the neural networks predictions
+        :X: is a numpy.ndarray with shape (nx, m) that contains
+        the input data
+        :Y: is a numpy.ndarray with shape (1, m) that contains
+        the correct labels for the input data
+        '''
+        self.forward_prop(X)
+        cost = self.cost(Y, self.__A2)
+        prediction = np.where(self.__A2 >= 0.5, 1, 0)
+        return prediction, cost
