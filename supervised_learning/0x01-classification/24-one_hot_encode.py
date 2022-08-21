9abfd4a6a015d7877ada5b01ab6d784694dcf871
luisobregon21 <luisitonicaragua2002@gmail.com>
2022-08-21 12:32:25 -0400

    add(supervised-learning): task 24
    met pycodestyle requirements

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -9,11 +9,12 @@ def one_hot_encode(Y, classes):
     converts a numerical label vector into a one-hot matrix
     :Y: is a numpy.ndarray with shape (m,) containing numeric class labels
     :classes: is the maximum number of classes found in Y
-    :Returns: a one-hot encoding of Y with shape (classes, m), or None on failure
+    :Returns: a one-hot encoding of Y
+    with shape (classes, m), or None on failure
     '''
     try:
         one_hot = np.zeros((classes, Y.shape[0]))
         one_hot[Y, np.arange(Y.shape[0])] = 1
         return one_hot
-    except:
+    except Exception as e:
         return None
