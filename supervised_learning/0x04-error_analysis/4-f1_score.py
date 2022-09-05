9e55bb6a44b854f63455644b9617cade7af1e5e1
Luis Obregon <luisitonicaragua2002@gmail.com>
2022-09-05 11:16:18 -0400

    add(0x04-error_analysis):  add task 4

---
+++ supervised_learning/0x04-error_analysis/4-f1_score.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''calculates the F1 score'''
+
+precision = __import__('2-precision').precision
+sensitivity = __import__('1-sensitivity').sensitivity
+
+
+def f1_score(confusion):
+    '''
+    calculates the F1 score of a confusion matrix
+    :confusion: is a confusion numpy.ndarray of shape (classes, classes) where row indices
+    represent the correct labels and column indices represent the predicted labels
+        classes: is the number of classes
+    '''
+    prec = precision(confusion)
+    recall = sensitivity(confusion)
+    f1 = 2 * (prec * recall) / (prec + recall)
+    return f1
