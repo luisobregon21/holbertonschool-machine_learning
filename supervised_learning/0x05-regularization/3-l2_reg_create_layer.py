f4633f8d95a0366b388a672f98ad04e86731b715
luisobregon21 <luisitonicaragua2002@gmail.com>
2022-09-08 13:10:33 -0400

    fix(0x05-regulazation): task 3
    add scale to layer weight initializer

--- supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -13,7 +13,7 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     :lambtha: the L2 regularization parameter
     :returns: the output of the new layer
     '''
-    init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    init = tf.keras.initializers.VarianceScaling(scale=2.0, mode='fan_avg')
     regularizer = tf.keras.regularizers.L2(lambtha)
     layer = tf.layers.Dense(units=n, activation=activation,
                             kernel_initializer=init,
