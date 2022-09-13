3a4c866cab87ffe497a6410644784f93138deec4
luisobregon21 <luisitonicaragua2002@gmail.com>
2022-09-13 19:57:04 -0400

    add(supervised-learning): 0x06 keras
    add task 5

---
+++ supervised_learning/0x06-keras/5-train.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+''' train a model with Keras'''
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size,
+                epochs, validation_data=None, verbose=True, shuffle=False):
+    '''
+    trains a model using mini-batch gradient decent.
+    :network: is the model to train
+    :data: is a numpy.ndarray of shape (m, nx) containing the input data
+    :labels: is a one-hot numpy.ndarray of shape (m, classes) containing
+    :batch_size: is the size of the batch used for mini-batch gradient descent
+    :epochs: is the number of passes through
+    :validation_data: is the data to validate the model with, if not None
+    data for mini-batch gradient descent
+    :verbose: is a boolean that determines
+    if output should be printed during training
+    :shuffle: is a boolean that determines
+    whether to shuffle the batches every epoch.
+    '''
+
+    return network.fit(data, labels,
+                       batch_size=batch_size,
+                       epochs=epochs,
+                       validation_data=validation_data,
+                       verbose=verbose, shuffle=shuffle)
