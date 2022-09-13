86deaa6253cf447f295680f4bc4939589c903809
Luis Obregon <luisitonicaragua2002@gmail.com>
2022-09-13 19:17:36 -0400

    add(supervised-learning): 0x06 keras new task
    add task 1 and fix pycodestyle for task 0

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -6,7 +6,8 @@ import tensorflow.keras as K
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     ''''
-    builds a neural network with the Keras library.
+    builds a neural network with the Keras library not using
+    the sequential class.
     :nx: is the number of input features to the network
     :layers: is a list containing the number of nodes
     in each layer of the network
@@ -16,17 +17,15 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     :keep_prob: is the probability that a node will be kept for dropout
     '''
 
-    model = K.Sequential()
+    inputs = K.Input(shape=(nx,))
     L2 = K.regularizers.l2(lambtha)
     for idx in range(len(layers)):
         if idx == 0:
-            model.add(K.layers.Dense(layers[idx], input_shape=(nx,),
-                                     activation=activations[idx],
-                                     kernel_regularizer=L2,
-                                     name='dense'))
+            output = K.layers.Dense(layers[idx],
+                                    activation=activations[idx],
+                                    kernel_regularizer=L2)(inputs)
         else:
-            model.add(K.layers.Dropout(1 - keep_prob))
-            model.add(K.layers.Dense(layers[idx], activation=activations[idx],
-                                     kernel_regularizer=L2,
-                                     name='dense_' + str(layers[idx])))
-    return model
+            dropout = K.layers.Dropout(1 - keep_prob)(output)
+            output = K.layers.Dense(layers[idx], activation=activations[idx],
+                                    kernel_regularizer=L2)(dropout)
+    return K.models.Model(inputs=inputs, outputs=output)
