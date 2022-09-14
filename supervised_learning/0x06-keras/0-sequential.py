5124ecfd4b316dc59c5d9051ba314e174a4f3291
Luis Obregon <luisitonicaragua2002@gmail.com>
2022-09-13 20:55:01 -0400

    add(supervised-learning): 0x06 keras new task
    clean up task 0

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -17,16 +17,12 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     '''
 
     model = K.Sequential()
-    L2 = K.regularizers.l2(lambtha)
+    regularizer = K.regularizers.L2(lambtha)
+
     for idx in range(len(layers)):
-        if idx == 0:
-            model.add(K.layers.Dense(layers[idx], input_shape=(nx,),
-                                     activation=activations[idx],
-                                     kernel_regularizer=L2,
-                                     name='dense'))
-        else:
+        if idx != 0:
             model.add(K.layers.Dropout(1 - keep_prob))
-            model.add(K.layers.Dense(layers[idx], activation=activations[idx],
-                                     kernel_regularizer=L2,
-                                     name='dense_' + str(idx)))
+        model.add(K.layers.Dense(layers[idx], input_shape=(nx, ),
+                  activation=activations[idx], kernel_regularizer=regularizer))
+
     return model
