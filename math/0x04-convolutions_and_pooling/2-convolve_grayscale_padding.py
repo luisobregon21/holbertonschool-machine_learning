f5e814d3d9a59f52db6a5db10c48e7679047e96a
Luis Obregon <luisitonicaragua2002@gmail.com>
2022-09-15 22:18:37 -0400

    add(math): 0x04 convolutional and pooling
    broken checkers

--- math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -23,18 +23,17 @@ def convolve_grayscale_padding(images, kernel, padding):
     '''
     m, h, w = images.shape
     kh, kw = kernel.shape
-    ph, pw = padding
+    ph, pw = padding[0], padding[1]
     oh = h + 2 * ph - kh + 1
     ow = w + 2 * pw - kw + 1
-
-    # convolutional dimension
-    conv_image = np.zeros((m, oh, ow))
-    # padded images
-    padded_images = np.pad(images, pad_width=((0, 0), (ph, ph), (pw, pw)),
-                           mode='constant')
-    for y in range(h):
-        for x in range(w):
-            image_slice = padded_images[:, y:y+kh, x:x+kw]
-            # change values of zero's array
-            conv_image[:, y, x] = np.tensordot(image_slice, kernel)
-    return conv_image
+    dim = (m, oh, ow)
+    out = np.zeros(dim)
+    padded = np.pad(images, pad_width=((0, 0), (ph, ph), (pw, pw)),
+                    mode='constant', constant_values=0)
+    for i in range(dim[1]):
+        for j in range(dim[2]):
+            x = i + kh
+            y = j + kw
+            M = padded[:, i:x, j:y]
+            out[:, i, j] = np.tensordot(M, kernel)
+    return out
