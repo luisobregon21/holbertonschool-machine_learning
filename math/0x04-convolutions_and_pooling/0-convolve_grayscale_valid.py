9f836fb181c88bb41abacc7910c71b798ccdedd3
Luis Obregon <luisitonicaragua2002@gmail.com>
2022-09-15 21:17:02 -0400

    deleted comments

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -22,12 +22,10 @@ def convolve_grayscale_valid(images, kernel):
     conv_w = (w - kw) + 1
     conv_h = (h - kh) + 1
 
-    # convolutional dimension
     conv_image = np.zeros((m, conv_w, conv_h))
 
     for y in range(conv_h):
         for x in range(conv_w):
             image_slice = images[:, y:y+kh, x:x+kw]
-            # change values of zero's array
             conv_image[:, y, x] = np.tensordot(image_slice, kernel)
     return conv_image
