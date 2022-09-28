32e6ad2ff624a1f47c52046c35bb76f9f186de9e
Luis Obregon <luisitonicaragua2002@gmail.com>
2022-09-28 17:03:05 -0400

    add task 0 and 1

---
+++ supervised_learning/0x08-deep_cnns/0-inception_block.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+'''Inception Block'''
+
+import tensorflow.keras as K
+
+
+def inception_block(A_prev, filters):
+    '''
+    builds an inception block.
+    :A_prev: is the output from the previous layer
+    :filters: is a tuple or list containing F1, F3R,
+    F3,F5R, F5, FPP, respectively:
+        F1: is the number of filters in the 1x1 convolution
+        F3R: is the number of filters in the 1x1 convolution before the 3x3 convolution
+        F3: is the number of filters in the 3x3 convolution
+        F5R: is the number of filters in the 1x1 convolution before the 5x5 convolution
+        F5: is the number of filters in the 5x5 convolution
+        FPP: is the number of filters in the 1x1 convolution after the max pooling (Note : The output shape after the max pooling layer is outputshape = math.floor((inputshape - 1) / strides) + 1)
+    '''
+    init = K.initializers.he_normal()
+    activation = 'relu'
+    F1, F3R, F3, F5R, F5, FPP = filters
+    
+    conv1 = K.layers.Conv2D(filters=F1, kernel_size=1, padding='same',
+                               activation=activation,
+                               kernel_initializer=init)(A_prev)
+
+    conv2 = K.layers.Conv2D(filters=F3R, kernel_size=1, padding='same',
+                                activation=activation,
+                                kernel_initializer=init)(A_prev)
+
+    conv3 = K.layers.Conv2D(filters=F3, kernel_size=3, padding='same',
+                               activation=activation,
+                               kernel_initializer=init)(conv2)
+
+    conv4 = K.layers.Conv2D(filters=F5R, kernel_size=1, padding='same',
+                                activation=activation,
+                                kernel_initializer=init)(A_prev)
+
+    conv5 = K.layers.Conv2D(filters=F5, kernel_size=5, padding='same',
+                               activation=activation,
+                               kernel_initializer=init)(conv4)
+
+    pooling = K.layers.MaxPooling2D(pool_size=[3, 3], strides=(1, 1),
+                                       padding='same')(A_prev)
+
+    layer_poolP = K.layers.Conv2D(filters=FPP, kernel_size=1, padding='same',
+                                  activation=activation,
+                                  kernel_initializer=init)(pooling)
+
+    mid_layer = K.layers.concatenate([conv1, conv3,
+                                      conv5, layer_poolP])
+    
+    return mid_layer
--- supervised_learning/0x08-deep_cnns/README.md
+++ supervised_learning/0x08-deep_cnns/README.md
@@ -1,5 +1,17 @@
 # 0x08. Deep Convolutional Architectures
 
+## 1x1 Convolution
+
+- looks at 1 pixel instead of the patch of the image.
+
+- regular convolutional is basically a linear classifier for the patch of the image.
+
+- adding a 1 by 1 convolution in the middle of the convolution, that is basically like having a mini NN running over the patch instead of linear classifier.
+
+- basically matrix multiply with few parameters.
+
+- makes model deeper and have more parameters without changing the the structure.
+
 ## Learning Objectives
 
 At the end of this project, you are expected to be able to explain to anyone, without the help of Google:
@@ -11,6 +23,7 @@ What is a skip connection?
 What is a bottleneck layer?
 
 What is the Inception Network?
+ Inception architecture, we use the 1x1 convolutional filters to reduce dimensionality in the filter dimension. These 1x1 conv layers can be used in general to change the filter space dimensionality (either increase or decrease) and in the Inception architecture we see how effective these 1x1 filters can be for dimensionality reduction, explicitly in the filter dimension space, not the spatial dimension space.
 
 What is ResNet? ResNeXt? DenseNet?
 
