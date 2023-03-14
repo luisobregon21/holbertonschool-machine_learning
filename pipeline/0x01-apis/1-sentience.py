244c76e10b3815dca9c7e650e162afa47998070c
Luis Obregon <luis@coquihealth.com>
2023-03-13 20:23:58 -0400

    fix task 1

--- pipeline/0x01-apis/1-sentience.py
+++ pipeline/0x01-apis/1-sentience.py
@@ -8,16 +8,18 @@ def sentientPlanets():
     returns the list of names of the home planets
     of all sentient species.
     '''
-    url = "https://swapi.dev/api/species/"
     planets = []
+    url = 'https://swapi-api.hbtn.io/api/species/'
     while url is not None:
-        response = requests.get(url)
-        data = response.json()
-        for species in data['results']:
-            if species['designation'] == "sentient":
-                if species['homeworld'] is not None:
-                    planet_url = species['homeworld']
-                    planet_data = requests.get(planet_url).json()
-                    planets.append(planet_data['name'])
-        url = data['next']
+        response = requests.get(url,
+                                headers={'Accept': 'application/json'},
+                                params={"term": 'specie'})
+        for specie in response.json()['results']:
+            if specie['classification'] == 'sentient' or \
+                    specie['designation'] == 'sentient':
+                if specie['homeworld'] is not None:
+                    homeworld = requests.get(specie['homeworld'])
+                    planets.append(homeworld.json()['name'])
+
+        url = response.json()['next']
     return planets
